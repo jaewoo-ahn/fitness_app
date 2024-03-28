e6bac7c33ad4b70a2d89089494d30bfc57c4ee29
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-03-28 19:16:46 +0900

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import Navbar from "@/scenes/navbar";
+
 function App() {
   return (
-    <div className="app">
-      <div></div>
-      <div></div>
+    <div className="app bg-gray-20">
+      <Navbar />
     </div>
   );
 }
