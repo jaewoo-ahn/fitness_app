d54d4cdcea5690dea53b2b9fd34e5fa5c42e92aa
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-03-28 19:43:09 +0900

    fix error

--- src/scenes/navbar/Links.tsx
+++ src/scenes/navbar/Links.tsx
@@ -10,7 +10,9 @@ const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
   const lowerCasePage = page.toLowerCase().replace(/ /g, "");
   return (
     <AnchorLink
-      className={`${selectedPage === lowerCasePage} ? "text-primary-500" : "transition duration-500 hover:text-primary-300"`}
+      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+        transition duration-500 hover:text-primary-300
+      `}
       href={`#${lowerCasePage}`}
       onClick={() => {
         setSelectedPage(lowerCasePage);
