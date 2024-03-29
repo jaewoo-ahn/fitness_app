5c710467d0337c3443f791405defd8d026fca55a
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-03-29 23:22:13 +0900

    benefits

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
@@ -16,17 +17,32 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* image and madin header  */}
-      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+      <motion.div
+        className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+        onViewportEnter={() => {
+          setSelectedPage(SelectedPage.Home);
+        }}
+      >
         {/* main header */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* headings */}
-          <div className="md:-mt-22">
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img alt="home page text" src={HomePageText} />
               </div>
             </div>
-          </div>
+          </motion.div>
           <p className="mt-8 text-sm">
             Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
             Studios to get the Body Shapes That you Dream of.. Get Your Dream
@@ -53,7 +69,7 @@ const Home = ({ setSelectedPage }: Props) => {
         <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
           <img alt="home page grapic" src={HomePageGraphic} />
         </div>
-      </div>
+      </motion.div>
       {/* sponsors */}
       {!isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
