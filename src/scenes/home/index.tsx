0c5f322444e101c3ce22b9cf4542a8195cc59bcf
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-03-29 21:01:39 +0900

    main page layout

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -16,46 +16,49 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* image and madin header  */}
-      <div>
+      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
         {/* main header */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* headings */}
-          <div>
-            <div>
-              <img alt="home page text" src={HomePageText} />
+          <div className="md:-mt-22">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
+                <img alt="home page text" src={HomePageText} />
+              </div>
             </div>
           </div>
-          <p>
+          <p className="mt-8 text-sm">
             Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
             Studios to get the Body Shapes That you Dream of.. Get Your Dream
             Body Now.
           </p>
+          {/* actions */}
+          <div className="mt-8 flex items-center gap-8 ">
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline"
+              onClick={() => {
+                setSelectedPage(SelectedPage.ContactUs);
+              }}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
         </div>
-        {/* actions */}
-        <div>
-          <ActionButton setSelectedPage={setSelectedPage}>
-            Join Now
-          </ActionButton>
-          <AnchorLink
-            className="text-sm font-bold bg-primary-500 underline"
-            onClick={() => {
-              setSelectedPage(SelectedPage.ContactUs);
-            }}
-            href={`#${SelectedPage.ContactUs}`}
-          >
-            <p>Learn More</p>
-          </AnchorLink>
-        </div>
+
         {/* image */}
-        <div>
+        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
           <img alt="home page grapic" src={HomePageGraphic} />
         </div>
       </div>
       {/* sponsors */}
       {!isAboveMediumScreens && (
-        <div>
-          <div>
-            <div>
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
               <img alt="redbull image" src={SponsorRedBull} />
               <img alt="forbes image" src={SponsorForbes} />
               <img alt="fortune image" src={SponsorFortune} />
