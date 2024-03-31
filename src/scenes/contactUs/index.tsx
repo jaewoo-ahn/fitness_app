32057d4d93405509371b9c1e94a11da14448971d
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-03-31 19:52:04 +0900

    contact with react-hook-form

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -8,6 +8,7 @@ import HText from "@/shared/HText";
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
+  const inputStyle = "mt-5 rounded-md w-full bg-primary-300 px-5 py-3 ";
   const {
     register,
     trigger,
@@ -63,8 +64,59 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             onSubmit={onSubmit}
             method="POST"
             action="https://formsubmit.co/ahnjaewoo3042@gmail.com"
-          ></form>
+          >
+            <input
+              type="text"
+              className={inputStyle}
+              placeholder="NAME"
+              {...register("name", { required: true, maxLength: 100 })}
+            />
+            {errors.name && (
+              <p className="mt-1 text-primary-500">
+                {errors.name.type === "required" && "This field is Required"}
+                {errors.name.type === "maxLength" && "Max Length is 100 har"}
+              </p>
+            )}
+            <input
+              type="text"
+              className={inputStyle}
+              placeholder="EMAIL"
+              {...register("email", {
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              })}
+            />
+            {errors.email && (
+              <p className="mt-1 text-primary-500">
+                {errors.email.type === "required" && "This field is Required"}
+                {errors.email.type === "pattern" && "invalid email address"}
+              </p>
+            )}
+            <textarea
+              rows={4}
+              cols={50}
+              className={inputStyle}
+              placeholder="MESSAGE"
+              {...register("message", {
+                required: true,
+                maxLength: 2000,
+              })}
+            />
+            {errors.message && (
+              <p className="mt-1 text-primary-500">
+                {errors.message.type === "required" && "This field is Required"}
+                {errors.message.type === "maxLength" && "Max Length is 100 har"}
+              </p>
+            )}
+            <button
+              type="submit"
+              className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+            >
+              SUBMIT
+            </button>
+          </form>
         </motion.div>
+        <motion.div></motion.div>
       </div>
     </section>
   );
