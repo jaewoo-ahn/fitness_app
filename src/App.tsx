d3875e567db6279fff7fd9d8b3cc580bd2721128
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-03-31 20:04:27 +0900

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourClasses";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@scenes/footer";
 import { useState, useEffect } from "react";
 import { SelectedPage } from "./shared/types";
-import OurClasses from "./scenes/ourClasses";
-import ContactUs from "./scenes/contactUs";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -35,6 +36,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,15 @@
+import Logo from "@/assets/Logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
